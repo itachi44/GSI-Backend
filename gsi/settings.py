2289781439a6ff270b9a9fa5f38ae80705f28d5a
Ahmadou_Bamba <diopbamba86@gmail.com>
2021-11-24 14:13:51 +0000

    ...

--- gsi/settings.py
+++ gsi/settings.py
@@ -59,6 +59,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication', 
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE':3
 }
 
 MIDDLEWARE = [
